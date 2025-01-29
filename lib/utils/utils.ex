48a503d421b6831b5f496d8ca5c7d41180eeaddd
Nicolas A. Serna <serna.nicolas.abel@gmail.com>
2025-01-29 17:32:03 -0300

    up: add utils module

---
+++ lib/utils/utils.ex
@@ -0,0 +1,29 @@
+defmodule Utils do
+  @moduledoc """
+  Some useful functions that can be used in the problems.
+  """
+
+  @spec prime?(number()) :: boolean()
+  def prime?(0), do: false
+
+  def prime?(1), do: false
+
+  def prime?(n) when n < 0, do: prime?(abs(n))
+
+  def prime?(n), do: prime?(n, 2)
+
+  defp prime?(n, n), do: true
+
+  defp prime?(n, p) when rem(n, p) == 0, do: false
+
+  defp prime?(n, p), do: prime?(n, p+1)
+
+  @spec composite?(number()) :: boolean()
+  def composite?(0), do: false
+
+  def composite?(1), do: false
+
+  def composite?(n) when n < 0, do: composite?(abs(n))
+
+  def composite?(n), do: not prime?(n)
+end
