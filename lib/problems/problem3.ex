651860b16e5697099d8400e89204614d8ab682c7
Nicolas A. Serna <serna.nicolas.abel@gmail.com>
2025-01-29 17:32:21 -0300

    up: solution to problem 3

--- lib/problems/problem3.ex
+++ lib/problems/problem3.ex
@@ -1,3 +1,32 @@
 defmodule ProjectEuler.Problem3 do
+  import Utils
 
+  @moduledoc """
+  The prime factors of 13195 are 5, 7, 13 and 29.
+
+  What is the largest prime factor of the number 600851475143?
+  -----------------------------------------------------------------
+
+  - Ref 1: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+  """
+
+  def get_primes(n) do
+    q = :math.sqrt(n) |> ceil
+    primes = 2..q
+    |> Stream.filter(&prime?/1)
+    primes
+  end
+
+  def find_largest_factor(n) do
+    get_primes(n)
+    |> Enum.reduce(-1, fn x, acc ->
+      if rem(n, x) == 0 and x > acc, do: x, else: acc
+    end)
+  end
+
+  # change n with 600851475143
+  def start(n \\ 13195) do
+    res = find_largest_factor(n)
+    IO.puts("The largest prime facot of #{n} is #{res}")
+  end
 end
